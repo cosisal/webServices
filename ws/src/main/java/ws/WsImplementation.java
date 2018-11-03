99de2903f0b6f5fed6fcf5b5a93f0d1ed143cf41
cosisal <cosisal@libero.it>
2018-11-03 19:24:22 +0100

    Ws SOAP initial import

---
+++ ws/src/main/java/ws/WsImplementation.java
@@ -0,0 +1,26 @@
+package ws;
+
+public class WsImplementation {
+
+	
+	public String diCiao() {
+		
+		System.out.println("Dico ciao a tutti");
+		
+		return "Ciao a tutti";
+	}
+	
+	public String sayHello() {
+		
+		System.out.println("Hello");
+		
+		return "Hello";
+	}
+	
+	public String diQualcosa(String cosaDico) {
+		
+		System.out.println("Mi hanno fatto dire "+cosaDico);
+		
+		return cosaDico;
+	}
+}
